9a2b37ddb48266013bfb509d83f752ee0d482949
LeoderProfi <87063329+LeoderProfi@users.noreply.github.com>
2024-11-14 12:22:12 +0100

    initial

---
+++ HW3_CPP_KTH.cpp
@@ -0,0 +1,12 @@
+﻿// HW3_CPP_KTH.cpp: Definiert den Einstiegspunkt für die Anwendung.
+//
+
+#include "HW3_CPP_KTH.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
